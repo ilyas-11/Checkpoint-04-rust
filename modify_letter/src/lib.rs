051675c557628d3ca2750df4dc1a7731fef22e49
ilyas-11 <ilyassabid11@gmail.com>
2026-07-18 14:31:25 +0100

    modify_letter

---
+++ modify_letter/src/lib.rs
@@ -0,0 +1,31 @@
+pub fn remove_letter_sensitive(s: &str, letter: char) -> String {
+    s.chars()
+        .filter(|&c| c != letter)
+        .collect()
+}
+
+pub fn remove_letter_insensitive(s: &str, letter: char) -> String {
+    let letter = letter.to_ascii_lowercase();
+
+    s.chars()
+        .filter(|&c| c.to_ascii_lowercase() != letter)
+        .collect()
+}
+
+pub fn swap_letter_case(s: &str, letter: char) -> String {
+    let letter = letter.to_ascii_lowercase();
+
+    s.chars()
+        .map(|c| {
+            if c.to_ascii_lowercase() == letter {
+                if c.is_ascii_lowercase() {
+                    c.to_ascii_uppercase()
+                } else {
+                    c.to_ascii_lowercase()
+                }
+            } else {
+                c
+            }
+        })
+        .collect()
+}
