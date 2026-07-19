9b7c5e9624f085805ed3c14a13f3457535dc9cff
ilyas-11 <ilyassabid11@gmial.com>
2026-07-19 23:27:55 +0100

    order_books

---
+++ order_books/src/lib.rs
@@ -0,0 +1,25 @@
+use crate::library::writers::Writer;
+
+pub mod library{
+    pub mod books{
+        pub struct Book{
+            pub title: String,
+            pub year: u32,
+        }
+    }
+    pub mod writers{
+        use crate::library::books::Book;
+        pub struct Writer{
+            pub first_name: String,
+            pub last_name: String,
+            pub books: Vec<Book>,
+        }
+    }
+}
+
+
+pub fn order_books(writer: &mut Writer) {
+    writer.books.sort_by(|a,b|{
+        a.title.to_lowercase().cmp(&b.title.to_lowercase())
+    });
+}
